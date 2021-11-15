bcf50bc8b992755ececc3a657fe7066d9bed35e9
manguila2525 <manguilas2525@gmail.com>
2021-11-15 18:36:53 -0400

    details in form contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,13 +20,10 @@ const Contact = () => {
         <form className="form__about col-12 col-md-6 p-5" onSubmit={sendEmail}>
           <h1 className="display-2 bold title__about my-2 text-white">Contact</h1>
           <div className="mb-3" >
-            <label htmlFor="name" className="form-label text-muted">Name:</label>
-            <input type="text" name="to_name" id="name" className="form-control" placeholder="Your name" />
+            <label htmlFor="name" className="form-label text-muted">Name and Email:</label>
+            <input type="text" name="to_name" id="name" className="form-control" placeholder="Your name and your email" />
           </div>
-          <div className="mb-3" >
-            <label htmlFor="from_name" className="form-label text-muted">Email:</label>
-            <input type="text" name="from_namee" id="from_name" placeholder="Your email" className="form-control" />
-          </div>
+
           <textarea name="message" placeholder="Send your message" className="form-control mb-3" cols="49" rows="10"></textarea>
           <button className="btn btn-primary btn-focus form-control" type="submit">Send</button>
         </form>
